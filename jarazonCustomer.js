16ee569032e5b036d8d0832a2f8cf70444e43452
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-04-26 11:59:17 -0600

    Displayed items to sell. Also giving an option for next steps to buy or add an item

---
+++ jarazonCustomer.js
@@ -0,0 +1,87 @@
+var mysql = require("mysql");
+
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: "jarazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
+  showProducts(intialPrompt);
+});
+
+var welcome =
+  `
+           _                      _       
+ _ _ _ ___| |___ ___ _____ ___   | |_ ___ 
+| | | | -_| |  _| . |     | -_|  |  _| . |
+|_____|___|_|___|___|_|_|_|___|  |_| |___|
+`
+
+var logo =
+  `                                                                                      
+     __                                    
+    |__.---.-.----.---.-.-----.-----.-----.
+    |  |  _  |   _|  _  |-- __|  _  |     |
+    |  |___._|__| |___._|_____|_____|__|__|
+   |___|                                   
+`;
+
+console.log(welcome + logo);
+
+// *Shows all objects to be sold
+function showProducts(fn) {
+  connection.query("select * from products", function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log(
+        `
+      ${res[i].id} Item: ${res[i].item} 
+      Dept: ${res[i].department_name} 
+      Quantity: ${res[i].quantity}`);
+
+    }
+    console.log(`     ----------------------------------`);
+    fn()
+  });
+}
+
+function intialPrompt() {
+
+  inquirer.prompt([{
+    message: "What would you like to do?",
+    type: "list",
+    choices: ["Buy my stuff", "Add an item"],
+    name: "option"
+  }]).then(function (res) {
+    switch (res.option) {
+      case "Buy my stuff":
+        showProducts(buy);
+        break;
+
+      case "Add an item":
+
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+// * Will ask which item to purchase
+function buy() {
+  inquirer.prompt
+}
