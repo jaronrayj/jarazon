ed783b7805e8cc8d0d613e658646e7eb37135912
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-04-26 11:00:27 -0600

    Input products to sell

---
+++ jarazon_content.sql
@@ -0,0 +1,19 @@
+use jarazon_db;
+-- Insert rows into table 'products'
+INSERT INTO products
+    ( -- columns to insert data into
+    item,price, department_name, quantity
+    )
+VALUES
+    ( "My shoe", 30, "Personal Household Goods", 11),
+    ( "Used Toothbrush", 7.50, "Personal Household Goods", 3),
+    ( "Children's artwork", 99.99, "Personal Household Goods", 78),
+    ( "Dented pots and pans", 19.97, "Personal Household Goods", 8),
+    ( "Nearly dead lawn mower", 6, "Garage Stuff", 1),
+    ( "Crusted paint brushes", 8, "Garage Stuff", 5),
+    ( "Ladder (actually a bucket)", 5, "Garage Stuff", 12),
+    ( "Mostly fresh underwear", 20, "Personal Household Goods", 15),
+    ( "Tony's jeans", 60, "Acquired Items", 3),
+    ( "Neighbor's Barbies", .5, "Acquired Items", 5);
+
+select * from products;
--- jarazon_structure.sql
+++ jarazon_structure.sql
@@ -4,10 +4,11 @@ create database jarazon_db;
 
 use jarazon_db;
 
-create table items(
+create table products(
     id int not null auto_increment,
 	item varchar(50) not null,
     price decimal(10,4) not null,
+    department_name VARCHAR(50),
     quantity int not NULL,
     primary key(id)
 
